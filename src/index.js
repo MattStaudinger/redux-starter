d6d8330783c763a4b0bdfe2529faef4360735632
Matthias Staudinger <staudinger.matthias@googlemail.com>
2020-05-23 20:27:36 +0200

    add first introduction of redux - store, actions, reducer

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log("Hello World!");
+import store from "./store";
+import { bugAdded, bugRemoved, bugResolved } from "./actions";
+const unsubscribe = store.subscribe(() => {
+  console.log("store changed", store.getState());
+});
+
+store.dispatch(bugAdded("Bug1"));
+store.dispatch(bugResolved(1));
+store.dispatch(bugRemoved(1));
